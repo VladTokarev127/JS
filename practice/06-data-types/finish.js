560b28147afb888bebee8b875582a259340e2792
Vladislav Tokarev <119095605+VladTokarev127@users.noreply.github.com>
2023-03-24 21:27:26 +0500

    Задачки

---
+++ practice/06-data-types/finish.js
@@ -0,0 +1,31 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+
+let str = '213123';
+let num = 213123;
+let bool = true;
+let newNull = null;
+let newUndefined = undefined;
+let obj = {};
+let arr = [];
+
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof bool)
+console.log(typeof newNull)
+console.log(typeof newUndefined)
+console.log(typeof obj)
+console.log(typeof arr)
